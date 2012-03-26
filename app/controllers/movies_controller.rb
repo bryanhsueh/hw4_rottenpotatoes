97c2e7ec15214996caa89ade96c0918528a8e534
saasbook <saasbook@saasbook.(none)>
2012-03-26 09:09:11 -0700

    add the method in model

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -61,11 +61,12 @@ class MoviesController < ApplicationController
   end
 
   def same_director
-    @movie = Movie.find params[:id]
-    if @movie.director and !@movie.director.empty?
-      @movies = Movie.find_all_by_director(@movie.director)
-    else
-      flash[:notice] = "'#{@movie.title}' has no director info."
+    id = params[:id]
+    @movie = Movie.find(id)
+    begin
+      @movies = Movie.find_same_director(id)
+    rescue Movie::NoDirectorInfo => exception
+      flash[:warning] = "'#{@movie.title}' has no director info."
       redirect_to movies_path
     end
   end
