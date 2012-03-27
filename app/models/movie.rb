0ace668893e53801e30e5bc24272f8683bae77df
saasbook <saasbook@saasbook.(none)>
2012-03-26 22:59:06 -0700

    add rspec and coverage

--- app/models/movie.rb
+++ app/models/movie.rb
@@ -3,12 +3,11 @@ class Movie < ActiveRecord::Base
     %w(G PG PG-13 NC-17 R)
   end
 
-  class Movie::NoDirectorInfo < StandardError;
-  end
+  class Movie::NoDirectorInfo < StandardError; end
 
   def self.find_same_director(id)
     movie = self.find(id)
-    director = movie.director
+    director = movie.respond_to?('director') ? movie.director : ''
     if director and !director.empty?
       self.find_all_by_director(director)
     else
